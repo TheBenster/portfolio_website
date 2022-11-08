9c80227934c6640e8bd9c1dd813aa90ebd1e6ba9
Ben Beaver <bbeaver93@gmail.com>
2022-11-07 21:53:27 -0500

    bootstrap, added filler text for navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import './test.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="head-header">
+        <h1>Hello, welcome to my site.</h1>
+      </div>
       <div className="App-header">
-        asdfasdfasfdasdfasdfasd
       </div>
       <h1>YOYOYOYOYOYO</h1>
     </div>
